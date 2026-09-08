0239c6ac0ccbfeb5758efd744f7dfa18b574ee80
Yuw-zhang <yuwei.zhang@stonybrook.edu>
2026-09-08 16:04:45 -0400

    Update streamlit_stitch.py

--- streamlit_stitch.py
+++ streamlit_stitch.py
@@ -245,7 +245,7 @@ if img_tl and img_bl and img_br and img_tr:
         canvas_center_x = slide_canvas.shape[1] // 2
         canvas_center_y = slide_canvas.shape[0] // 2
 
-        gap_factor = 0.9
+        gap_factor = 0.95
 
         left_height = (info_dict[1]['height']+ info_dict[1]['height'])/2
         right_height = (info_dict[2]['height']+ info_dict[2]['height'])/2
